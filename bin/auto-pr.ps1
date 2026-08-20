d410623a7a456c40561a0edea71d8a7cbbb08b85
aios789 <317836464+aios789@users.noreply.github.com>
2026-08-20 13:52:52 +0800

    feat: add new app

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "/scoop-bucket:main"
+    [String]$upstream = "aios789/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
